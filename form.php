3f18444415e491030235e6ef51f7f792fb88aa4d
sarvesh-j <sarvesh.ewps@gmail.com>
2021-09-30 23:40:50 +0530

    Add files via upload
    initial

--- form.php
+++ form.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "sarvesh.ewps@gmail.com";
 $subject = "Mail From Protfolio website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@myportfolio.com" . "\r\n" .
+$headers = "From: silencevalo1@gmail.com" . "\r\n" .
 
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
